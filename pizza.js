8a10341eb89ffa070679e4aa313c3befa04e283a
RUnroe <ryanunroe@gmail.com>
2020-11-12 12:35:10 -0700

    calculatePizzaPrice method and made pizzaData object

--- pizza.js
+++ pizza.js
@@ -1,1 +1,68 @@
-// Object holding topings, prices, and pre-built pizzas 
+// Object holding toppings, prices, and pre-built pizzas 
+const pizzaData = {
+    toppings: [
+        {name: "pepperoni", imgSrc:""},
+        {name: "olive", imgSrc:""},
+        {name: "sausage", imgSrc:""},
+        {name: "mushroom", imgSrc:""},
+        {name: "ham", imgSrc:""},
+        {name: "jalapeno", imgSrc:""},
+        {name: "marshmallow", imgSrc:""},
+        {name: "pizza roll", imgSrc:""},
+        {name: "cinnamon", imgSrc:""},
+        {name: "avocado", imgSrc:""}
+    ],
+    prices:{
+        "sm": 6.99,
+        "md": 9.99,
+        "lg": 12.99,
+        "xl": 16.99
+    },
+    prebuilt:{
+        pepperoniPizza: {
+            toppings: [
+                {name: "pepperoni", position: "all", quantity:"full"}
+            ]
+        },
+        cowboy: {
+            toppings: [
+                {name: "pepperoni", position: "all", quantity:"full"},
+                {name: "sausage", position: "all", quantity:"full"},
+                {name: "olive", position: "all", quantity:"full"},
+                {name: "mushroom", position: "all", quantity:"full"}
+            ]
+        },
+        vowelPizza: {
+            toppings: [
+                {name: "avocado", position: "all", quantity:"full"},
+                {name: "olives", position: "all", quantity:"full"}
+            ]
+        },
+        allMeatTreatPizza: {
+            toppings: [
+                {name: "pepperoni", position: "all", quantity:"full"},
+                {name: "sausage", position: "all", quantity:"full"},
+                {name: "ham", position: "all", quantity:"full"},
+                {name: "marshmallow", position: "all", quantity:"full"}
+            ]
+        },
+        spookyPizza: {
+            toppings: [
+                {name: "cinnamon", position: "all", quantity:"full"},
+                {name: "avocado", position: "all", quantity:"full"},
+                {name: "jalapeno", position: "all", quantity:"full"}
+            ]
+        }
+    }
+}
+
+
+
+//Model of built pizza
+// let objectName = {
+//     size: "sm",
+//     toppings: [
+//         {name: "pepperoni", position: "left", quantity:"full"}, 
+//         {name: "olives", position: "all", quantity:"double"} 
+//     ]
+// }
--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //General script file
+
+//Pass in pizza object (see pizza.js for example model) and this method will return the price.
+const calculatePrice = pizzaObject => {
+    //Base price based on size
+    let price = pizzaData.prices[pizzaObject.size];
+    //For each topping listed: Add 1 to price if quantity == "full", 2 if quantity == "double"
+    for(topping of pizzaObject.toppings) {
+        if(topping.quantity == "full") price += 1;
+        else if(topping.quantity == "double") price += 2;
+    }
+    return price;
+
+}
+
+//Test code to verify price calculation works
+// let objectName = {
+//     size: "sm",
+//     toppings: [
+//         {name: "pepperoni", position: "left", quantity:"full"}, 
+//         {name: "olives", position: "all", quantity:"double"} 
+//     ]
+// }
+
+// console.log(calculatePrice(objectName));
